3df9fb3921d10b317e5886977f5e20921fd76ad1
Sebastian Agudelo <287745321+borna-trustcore@users.noreply.github.com>
2026-08-26 01:47:38 -0500

    feat: initialize Spring Boot project with hexagonal architecture, MySQL support, and standardized API response handling.

--- market_place/src/main/java/application/MarketPlaceApplication.java
+++ market_place/src/main/java/application/MarketPlaceApplication.java
@@ -2,8 +2,10 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"application", "domain", "adapter", "config", "shared"})
 public class MarketPlaceApplication {
 
 	public static void main(String[] args) {
